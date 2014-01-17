55b6083fe1c38c5b3aac738b9c0d9299b91209a3
Greg Roberts <gregorysroberts@gmail.com>
2014-01-16 20:36:47 -0700

    Adds prepend method without formating

--- js/main.js
+++ js/main.js
@@ -12,10 +12,19 @@ $(function(){
 
 		if (count >= 130 ) {
 			$('#char-count').css('color', 'red');
+			$('.button').attr('disabled', 'disabled');
 		} else {
 			$('#char-count').css('color', 'black');
+			$('.button').removeAttr('disabled', 'disabled');
 		}
-		
+	});
+
+	$('.button').click(function(){
+		//<!-- STEP 5: When the user successfully inputs characters and clicks the “Tweet” button, a new 
+		//tweet should be created and added to the tweet stream in the main column, using the user’s fake 
+		//profile image in the top left and username/fullname. -->
+		//<!-- HINT: jQuery ".prepend" method -->
+		$('#stream').prepend('<p>Greg</p>');
 
 	});
 
